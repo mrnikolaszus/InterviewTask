64f4223ffa93eff83c6fef6624c85c62669f0c2e
mister1burger <mister1burger@vk.com>
2023-04-08 15:46:37 +0300

    исправления вывода

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,17 @@ package org.example;
 
 import org.example.services.DataService;
 
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
 public class Main {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        
         Object convertedData = new DataService().getConvertedData();
-
-        System.out.println(convertedData.toString());
+        
+        PrintStream out = new PrintStream(System.out, true, "UTF-8");
+        
+        out.println(convertedData);
     }
 }
